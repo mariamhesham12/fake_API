fa4279a2cdf683f54a9b85e0d8ec32211a508c1c
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-03-16 15:37:45 +0200

    third

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import userRouter from './src/modules/user/user.router.js'
+import dotenv from'dotenv'
+import cors from 'cors'
 const app=express()
-const port=3000
+dotenv.config()
+const port=+process.env.PORT
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)
