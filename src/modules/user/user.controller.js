f00a44c3efa89762930af52335b07f2c2c714855
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-04-20 20:51:52 +0200

    store

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -154,9 +154,23 @@ const home=async(req,res,next)=>{
         }
         })
 }
+
+const store=async(req,res,next)=>{
+    return res.json({
+        status: 0,
+        message: "Success",
+        image:"https://images.unsplash.com/photo-1562280963-8a5475740a10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+        id:1,
+        title:"Amazing Stores",
+        details:"Details Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        services:"Services Store Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        about:"About Store Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    })
+}
 export{
     login,
     register,
     forgetPassword,
-    home
+    home,
+    store
 }
--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,8 +1,9 @@
 import Router from 'express'
-import { forgetPassword, home, login, register } from './user.controller.js'
+import { forgetPassword, home, login, register, store } from './user.controller.js'
 const userRouter=Router()
 userRouter.post('/login',login)
 userRouter.post('/register',register)
 userRouter.post('/forgetPassword',forgetPassword)
 userRouter.get('/home',home)
+userRouter.get('/storeDetails/1',store)
 export default userRouter
